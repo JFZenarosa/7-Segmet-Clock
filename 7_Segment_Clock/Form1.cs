a129bf269a3de423453e4ec811b433a380e4a4e9
JFZenarosa <jaireh62@gmail.com>
2021-12-06 17:21:30 +0800

    0-9 are now printing out in the ones part of the seconds, will start with the tens part of the seconds

--- 7_Segment_Clock/Form1.cs
+++ 7_Segment_Clock/Form1.cs
@@ -42,6 +42,105 @@ namespace _7_Segment_Clock
                 S1F.PerformClick();
                 S1G.UseVisualStyleBackColor = true;
             }
+
+            else if(seconds == 1)
+            {
+                S1A.UseVisualStyleBackColor = true;
+                S1B.PerformClick();
+                S1C.PerformClick();
+                S1D.UseVisualStyleBackColor = true;
+                S1E.UseVisualStyleBackColor = true;
+                S1F.UseVisualStyleBackColor = true;
+                S1G.UseVisualStyleBackColor = true;
+            }
+
+            else if (seconds == 2)
+            {
+                S1A.PerformClick();
+                S1B.PerformClick();
+                S1C.UseVisualStyleBackColor = true;
+                S1D.PerformClick();
+                S1E.PerformClick();
+                S1F.UseVisualStyleBackColor = true;
+                S1G.PerformClick();
+            }
+
+            else if (seconds == 3)
+            {
+                S1A.PerformClick();
+                S1B.PerformClick();
+                S1C.PerformClick();
+                S1D.PerformClick();
+                S1E.UseVisualStyleBackColor = true;
+                S1F.UseVisualStyleBackColor = true;
+                S1G.PerformClick();
+            }
+
+            else if (seconds == 4)
+            {
+                S1A.UseVisualStyleBackColor = true;
+                S1B.PerformClick();
+                S1C.PerformClick();
+                S1D.UseVisualStyleBackColor = true;
+                S1E.UseVisualStyleBackColor = true;
+                S1F.PerformClick();
+                S1G.PerformClick();
+            }
+
+            else if (seconds == 5)
+            {
+                S1A.PerformClick();
+                S1B.UseVisualStyleBackColor = true;
+                S1C.PerformClick();
+                S1D.PerformClick();
+                S1E.UseVisualStyleBackColor = true;
+                S1F.PerformClick();
+                S1G.PerformClick();
+            }
+
+            else if (seconds == 6)
+            {
+                S1A.PerformClick();
+                S1B.UseVisualStyleBackColor = true;
+                S1C.PerformClick();
+                S1D.PerformClick();
+                S1E.PerformClick();
+                S1F.PerformClick();
+                S1G.PerformClick();
+            }
+
+            else if (seconds == 7)
+            {
+                S1A.PerformClick();
+                S1B.PerformClick();
+                S1C.PerformClick();
+                S1D.UseVisualStyleBackColor = true;
+                S1E.UseVisualStyleBackColor = true;
+                S1F.UseVisualStyleBackColor = true;
+                S1G.UseVisualStyleBackColor = true;
+            }
+
+            else if (seconds == 8)
+            {
+                S1A.PerformClick();
+                S1B.PerformClick();
+                S1C.PerformClick();
+                S1D.PerformClick();
+                S1E.PerformClick();
+                S1F.PerformClick();
+                S1G.PerformClick();
+            }
+
+            else if (seconds == 9)
+            {
+                S1A.PerformClick();
+                S1B.PerformClick();
+                S1C.PerformClick();
+                S1D.PerformClick();
+                S1E.UseVisualStyleBackColor = true;
+                S1F.PerformClick();
+                S1G.PerformClick();
+            }
         }
     }
 }
